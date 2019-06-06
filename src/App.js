05d3237224d0f6a58045349945c930ad29bc7f19
lusalga <ladsaldanha@gmail.com>
2019-06-06 11:32:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className = "bigcontainer">
+    
+      <div className="smallerbox">
+      	<input type="checkbox" name="activity" />
+      	<p>Grocery Shopping</p>
+      </div>
+
+      <div className="smallerbox">
+      	<input type="checkbox" name="activity" />
+      	<p>Work out</p>
+      </div>
+
+      <div className="smallerbox">
+      	<input type="checkbox" name="activity" />
+      	<p>Walk the dog</p>
+      </div>
+
+      <div className="smallerbox">
+      	<input type="checkbox" name="activity" />
+      	<p>Read your book</p>
+      </div>
+
+    </div>    
   );
 }
 
