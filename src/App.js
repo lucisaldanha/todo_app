1dba144ba39a20bca0ec6f451bf2d5b7ddbde3de
lusalga <ladsaldanha@gmail.com>
2019-06-12 07:47:57 -0500

    added map method to list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import TodoItem from './TodoItem.js';
+import todoData from './todoData';
+
 
 function App() {
+	const todoItemComponents = todoData.map(dataItem => { 
+		return <TodoItem key={dataItem.id} texto={dataItem.text} completo={dataItem.completed} />
+	})
   return (
     <div className = "list">
-    	<TodoItem />
-    	<TodoItem />
-     	<TodoItem />
-    	<TodoItem />
+    	{todoItemComponents}    	    	
     </div>    
   )
 }
