5270452f13e294997eeb1b7f0a53c1cb8bfd3741
lusalga <ladsaldanha@gmail.com>
2019-06-18 16:28:05 -0500

    Added handleChange method to App and checked if working

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,7 +4,11 @@ import './TodoItem.css';
 function TodoItem(props) {
 	return (
 		<div className = "items">
-	      	<input type= "checkbox" checked= {props.completo}/>
+	      	<input 
+		      	type = "checkbox" 
+		      	checked = {props.completo}
+		      	onChange = {() => props.handleChange(props.id)} 
+	      	/>
 	      	<p>{props.texto}</p>
      	</div>
 	)
