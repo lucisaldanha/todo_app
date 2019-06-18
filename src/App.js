b12a7b054d16cca87d7bfde432a7265f738c586c
lusalga <ladsaldanha@gmail.com>
2019-06-18 12:04:50 -0500

    adding class component and state to App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import './App.css';
 import TodoItem from './TodoItem.js';
 import todoData from './todoData';
 
-
-function App() {
-	const todoItemComponents = todoData.map(dataItem => { 
-		return <TodoItem key={dataItem.id} texto={dataItem.text} completo={dataItem.completed} />
-	})
-  return (
-    <div className = "list">
-    	{todoItemComponents}    	    	
-    </div>    
-  )
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state={
+			todos: todoData
+		}
+	}
+	render() {
+		const todoItemComponents = this.state.todos.map(dataItem => { 
+			return <TodoItem key={dataItem.id} texto={dataItem.text} completo={dataItem.completed} />
+			})
+		return (
+		    <div className = "list">
+		    	{todoItemComponents}    	    	
+		    </div>    
+  		)
+	}
 }
 
 export default App;
+
+
+
+// function App() {
+// 	const todoItemComponents = todoData.map(dataItem => { 
+// 		return <TodoItem key={dataItem.id} texto={dataItem.text} completo={dataItem.completed} />
+// 	})
+//   return (
+//     <div className = "list">
+//     	{todoItemComponents}    	    	
+//     </div>    
+//   )
+// }
+ 
+
